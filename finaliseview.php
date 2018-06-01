a1590eed8e1fda1a7250236bb61e9d4f2e4bd037
Sharad Ghimire <sharadghimire5551@gmail.com>
2018-06-02 00:07:30 +1000

    Finalize Page Added

---
+++ finaliseview.php
@@ -0,0 +1,24 @@
+<?php 
+
+require_once("nocache.php");
+
+session_start();
+
+if(!_SESSION["who"]){
+	header("location: logoff.php");
+}
+
+if ($_SESSION["mode"] != 1) {   //Check access control
+	header("location: unauth.php");
+}
+
+
+?>
+<?php include("header.php"); ?>
+
+<body>
+    <p>Only staffs can access </p>
+
+</body>
+
+</html>
